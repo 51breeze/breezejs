98a3c579acd22a1c4c30be080f6080b1f71f0b43
51breeze <664371281@qq.com>
2015-04-12 14:08:16 +0800

    breezejs
    v1.0.0 bate

---
+++ BindData.js
@@ -0,0 +1,174 @@
+/**
+ * Created by Administrator on 15-3-14.
+ */
+(function(){
+
+    /**
+     * 数据双向绑定器
+     * @constructor
+     */
+    function BindData( target )
+    {
+        if( !(this instanceof BindData) )
+            return new BindData( target );
+
+        //初始化父类
+        Breeze.EventDispatcher.call(this);
+
+        /**
+         * @private
+         * @type {Breeze.Dictionary}
+         */
+        var subscription= new Breeze.Dictionary()
+            ,dataset={}
+            ,self=this
+
+        /**
+         * 提交属性到每个绑定的对象
+         * @param property
+         * @param newValue
+         */
+        ,commit=function(property,newValue)
+        {
+            var i,item,object,properties,targets= subscription.getAll();
+            for( i in targets )
+            {
+                item=targets[i];
+                if( item.key && item.value )
+                {
+                    object=item.key;
+                    properties=item.value;
+
+                    if( properties )
+                    {
+                        var callback=properties[property] || properties['*'];
+                        if( Breeze.isFunction( callback ) )
+                        {
+                            callback.call(object,property,newValue);
+
+                        }else if( property in properties || '*' in properties )
+                        {
+                            if( Breeze.isHTMLElement(object) )
+                            {
+                                property=Breeze.isFormElement( object ) ? 'value' : 'innerHTML' ;
+                                setAttr(object,property,newValue);
+
+                            }else if( object instanceof Breeze.BindData || object instanceof Breeze )
+                            {
+                                object.setProperty(property,newValue)
+
+                            } else if( Breeze.isObject(object,true) )
+                            {
+                                object[ property ]= newValue;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //监听当前绑定对象的属性。
+        if( !(target instanceof Breeze.EventDispatcher) )
+            target=Breeze.EventDispatcher.call( this,target );
+
+        target.addEventListener(Breeze.PropertyEvent.PROPERTY_CHANGE,function(event)
+        {
+            if( event instanceof Breeze.PropertyEvent )
+                self.setProperty(event.property,event.newValue );
+        });
+
+        /**
+         * 绑定需要动态改变属性的对象
+         * @param target
+         * @param property
+         * @returns {boolean}
+         */
+        this.bind=function(target,property,callback)
+        {
+            property = property || 'value';
+            if( Breeze.isString( target ) )
+            {
+                var i,items=Sizzle(target);
+                for( i in items )
+                    this.bind(items[i],property,callback);
+
+            }else if( Breeze.isObject(target,true) )
+            {
+                var obj = subscription.get(target)
+                if( !obj )subscription.set(target, (obj={}) );
+                obj[property]=callback;
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * 解除绑定
+         * @param target
+         * @param property
+         * @returns {boolean}
+         */
+        this.unbind=function(target,property)
+        {
+            var obj;
+            if( target && ( obj=subscription.get( target ) ) )
+            {
+                Breeze.isDefined(property) ? delete obj[ property ] : subscription.remove(target);
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * 设置数据源
+         * @param name
+         * @param value
+         */
+        this.data=function(name,value)
+        {
+            Breeze.isObject(name,true) ?  dataset=name : dataset[name]=value;
+        }
+
+        /**
+         * 设置属性
+         * @param name
+         * @param value
+         */
+        this.setProperty=function(name,value)
+        {
+            if( this.getProperty( name ) !== value )
+            {
+                dataset[ name ] = value;
+                commit(name,value);
+                var ev=new Breeze.PropertyEvent(Breeze.PropertyEvent.PROPERTY_CHANGE)
+                ev.property=name;
+                ev.newValue=value;
+                this.dispatchEvent(ev);
+            }
+        }
+
+        /**
+         * 获取属性
+         * @param name
+         * @returns {*}
+         */
+        this.getProperty=function(name)
+        {
+            return dataset[ name ];
+        }
+
+        /**
+         * 检查是否有指定的属性名
+         * @param name
+         * @returns {boolean}
+         */
+        this.hasProperty=function(name)
+        {
+            return dataset.hasOwnProperty(name);
+        }
+    }
+    BindData.prototype=new Breeze.EventDispatcher();
+    BindData.prototype.constructor=BindData;
+    Breeze.BindData=BindData;
+
+})()
---
+++ Dictionary.js
@@ -0,0 +1,108 @@
+/**
+ * Created by Administrator on 15-3-14.
+ */
+(function(){
+
+
+    /**
+     * 可以使用非字符串作为键值的存储表
+     * @constructor
+     */
+    function Dictionary()
+    {
+        if( !(this instanceof Dictionary) )
+            return new Dictionary();
+
+        var map=new Array()
+            ,indexByKey=function(key)
+            {
+                var i = 0,len=map.length,isscalar=Breeze.isScalar(key);
+                for(; i<len ; i++) if( ( isscalar && i===key ) || ( !isscalar && map[i] && map[i].key===key ) )
+                    return i;
+                return -1;
+            }
+
+        /**
+         * 设置指定键值的数据,如果相同的键值则会覆盖之前的值。
+         * @param key
+         * @param value
+         * @returns {*}
+         */
+        this.set=function(key,value)
+        {
+            if( Breeze.isScalar( key ) )
+                return map[ key.toString() ]=value;
+            this.remove( key );
+            map.push({'key':key,'value':value})
+            return value;
+        }
+
+        /**
+         * 获取已设置的值
+         * @param key
+         * @returns {*}
+         */
+        this.get=function( key )
+        {
+            if( Breeze.isScalar(key) )
+                return map[ key.toString() ];
+            var index=indexByKey(key);
+            return index >=0 ?  map[ index ].value : null ;
+        }
+
+        /**
+         * 返回所有已设置的数据
+         * 数组中的每个项是一个对象
+         * @returns {Array}
+         */
+        this.getAll=function()
+        {
+            return map;
+        }
+
+        /**
+         * 返回有的key值
+         * @returns {Array}
+         */
+        this.toKeys=function()
+        {
+            var value=[],i
+            for( i in map ) value.push( typeof map[i] ==='object' ? map[i].key : i );
+            return value;
+        }
+
+        /**
+         * 删除已设置过的对象,并返回已删除的值（如果存在）否则为空。
+         * @param key
+         * @returns {*}
+         */
+        this.remove=function( key )
+        {
+            var value=null;
+            if( Breeze.isScalar(key) )
+            {
+                key=key.toString();
+                value=map[ key ];
+                delete map[ key ];
+            }else
+            {
+                var index= indexByKey( key );
+                if( index >=0  ){
+                    value=map[index].value;
+                    map.splice(index,1);
+                }
+            }
+            return value;
+        }
+
+        /**
+         * 返回已设置数据的总数
+         * @returns {Number}
+         */
+        this.count=function()
+        {
+            return map.length;
+        }
+    }
+    Breeze.Dictionary=Dictionary;
+})()
