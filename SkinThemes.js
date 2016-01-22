fbbf8493c92b1fad76d51c3df03347243c946a2e
yejun <yejun@hadlink.com>
2016-01-22 19:19:03 +0800

    skin Themes

--- SkinThemes.js
+++ SkinThemes.js
@@ -8,28 +8,119 @@
 
     var themes={
 
-
         'default':{
+            //主要的颜色
+            'primary':{
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+            //辅助的颜色
+            'secondary':{
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+            //阅读的颜色
+            'standard':{
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
 
-             'backgroud': {
-                 'subject': '',  //主题颜色
-                 'secondary': '', //辅助颜色
-                 'standard': ''   //标准颜色
-             },
-            'text': {
-                 'subject': '',  //主题颜色
-                 'secondary': '', //辅助颜色
-                 'standard': ''   //标准颜色
-             },
-            'border': {
-                 'subject': '',  //主题颜色
-                 'secondary': '', //辅助颜色
-                 'standard': ''   //标准颜色
-             }
+            //成功颜色
+            'success' :{
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+
+            //错误颜色
+            'error' :{
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+
+            //警告颜色
+            'warn' :{
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+
+            //提示颜色
+            'prompt' :{
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+            //滑过颜色
+            'over' :{
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+            //滑出颜色
+            'out'  : {
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+            //松开颜色
+            'up': {
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+            //按下颜色
+            'down' : {
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+            //当前活动的颜色
+            'active' :{
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            },
+            //禁用颜色
+            'disable' : {
+                backgroud:'',
+                text:'',
+                border:'',
+                gradient:'',
+                shadow:''
+            }
         }
-
-
     };
+
+
     var SkinThemes={
 
         'get':function(name)
@@ -40,11 +131,9 @@
         {
             themes[name]=skinObject;
         }
-
     }
 
-    window.SkinThemes=SkinThemes
-
+    window.SkinThemes=SkinThemes;
 
 })(window)
 
